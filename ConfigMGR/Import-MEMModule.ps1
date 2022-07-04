e701afa92ed1ad9979229d95bae9eaf27ce93115
BBultitude <Bryan.Bultitude@gmail.com>
2022-07-04 12:04:32 +1000

    Added output to display module version

--- ConfigMGR/Import-MEMModule.ps1
+++ ConfigMGR/Import-MEMModule.ps1
@@ -17,6 +17,7 @@ Function Import-MEMModule {
       Purpose/Change: 25/06/2021 - Bryan Bultitude - Initial script development
                       21/09/2021 - Bryan Bultitude - Updated Function name to be different from Configuration Module Functions
                       09/12/2021 - Bryan Bultitude - Moved Comment Based Help to top of function
+                      04/07/2022 - Bryan Bultitude - Added output to display module version
    .EXAMPLE
       Import-MEMModule A00
    .EXAMPLE
@@ -24,11 +25,15 @@ Function Import-MEMModule {
    #>
    param ([Parameter(Mandatory = $true)]$SiteCode)
    if (-not (Test-Path -Path "$SiteCode`:")) {
+      $CCMModuleInfo = Import-PowerShellDataFile -Path (Join-Path (Split-Path $env:SMS_ADMIN_UI_PATH -parent) ConfigurationManager.psd1)
       Write-Host "$(Get-Date):   ConfigMgr module has not been imported yet, will import it now." -ForegroundColor Green
+      Write-Host "$(Get-Date):   ConfigMgr module version is $($CCMModuleInfo.ModuleVersion)" -ForegroundColor Green
       Import-Module (Join-Path (Split-Path $env:SMS_ADMIN_UI_PATH -parent) ConfigurationManager.psd1) | Out-Null
       Set-Location "$($SiteCode):" | Out-Null
    }
    elseif (Test-Path -Path "$SiteCode`:") {
+      $CCMModuleInfo = Import-PowerShellDataFile -Path (Join-Path (Split-Path $env:SMS_ADMIN_UI_PATH -parent) ConfigurationManager.psd1)
+      Write-Host "$(Get-Date):   ConfigMgr module version is $($CCMModuleInfo.ModuleVersion)" -ForegroundColor Green
       Write-Host "$(Get-Date):   ConfigMgr module has already been imported" -ForegroundColor DarkYellow
       Set-Location "$($SiteCode):" | Out-Null
    }
