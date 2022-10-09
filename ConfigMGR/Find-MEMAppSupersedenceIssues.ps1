e8965fd93aeec76f19e2213d08d52d8effa33fb8
BBultitude <Bryan.Bultitude@gmail.com>
2022-10-10 09:46:25 +1000

    Added switch and modified sections to allow search for Patch My PC Apps. Also added ETA

--- ConfigMGR/Find-MEMAppSupersedenceIssues.ps1
+++ ConfigMGR/Find-MEMAppSupersedenceIssues.ps1
@@ -9,17 +9,27 @@ Function Find-MEMAppSupersedenceIssues {
     .OUTPUTS
        N/A
     .NOTES
-       Version:        1.1
+       Version:        1.2
        Author:         Bryan Bultitude
        Creation Date:  15/03/2022
        Purpose/Change: 15/03/2022 - Bryan Bultitude - Initial script development
                        04/05/2022 - Bryan Bultitude - Removed search for IsSuperseding -eq "True" as only True if deployed, added search for superseeded app being distributed, changed initial application search to be alphabetical, added additional text to write-host.
+                       10/10/2022 - Bryan Bultitude - Added switch and modified sections to allow search for Patch My PC Apps. Also added ETA
     .EXAMPLE
        PS> Find-MEMAppSupersedenceIssues
     #>
+    param (
+        [Parameter(Mandatory = $False)]
+        [Switch]$PMP = $False
+    )
     Import-MEMModule A00
-    $Applications = Get-CMApplication -Fast | Sort-Object -Property LocalizedDisplayName
+    Switch ($PMP) {
+        $true { $Applications = Get-CMApplication -Fast | Where-Object { $_.CreatedBy -eq "BMD\sa-MEM-PatchMyPC01" } | Sort-Object -Property LocalizedDisplayName }
+        $false { $Applications = Get-CMApplication -Fast | Sort-Object -Property LocalizedDisplayName }
+    }
+    $AllApplications = Get-CMApplication -Fast | Sort-Object -Property LocalizedDisplayName
     $x = $Applications.Count
+    Write-Host "Approximate completion will be $((Get-Date).AddMinutes($(($x * 2)/60)))" -ForegroundColor Red
     $i = 0
     Foreach ($Application in $Applications) {
         try {
@@ -30,7 +40,7 @@ Function Find-MEMAppSupersedenceIssues {
                 $CurrentSI = ([xml]$si.InnerXml).DeploymentTypeIntentExpression.DeploymentTypeApplicationReference
                 $supersedeAPP = "$($CurrentSI.AuthoringScopeId)/$($CurrentSI.LogicalName)"
                 $OldApp = ""
-                Foreach ($AllAPPS in $Applications) {
+                Foreach ($AllAPPS in $AllApplications) {
                     If ($supersedeAPP -eq $($AllAPPS.ModelName)) {
                         $OldApp = $AllAPPS.LocalizedDisplayName
                     }
